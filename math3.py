627eb1e518f4f8c1c5a776f990555bd28019645a
Dror Madar <drormadardm@gmail.com>
2022-04-15 12:53:05 +0300

    #1 third add third script as second

---
+++ math3.py
@@ -0,0 +1,13 @@
+print("Yoav Learns math")
+
+#result=8000*2000
+#print("10*10=",10*10)
+#print("8000*2000=",result)
+
+x=3
+y=7
+result=x**y
+print("x**y=",result)
+
+result=y**x
+print("y**x=",result)
