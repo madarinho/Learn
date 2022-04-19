682a15aad7f84f685a2c87cf16fc88cb1bb6f423
Dror Madar <drormadardm@gmail.com>
2022-04-19 14:06:10 +0300

    #2 Deleted redundant math modules. added the calculator module and a unitests for it.

---
+++ calculator1/source1/calculator.py
@@ -0,0 +1,22 @@
+class Calculator(object):
+
+    @staticmethod
+    def multiply(x,y):
+        return x*y
+
+    @staticmethod
+    def power(x,y):
+        return x**y
+
+    @staticmethod
+    def divide(x,y):
+        return x/y
+
+# x=int(input("x: "))
+# y=int(input("y: "))
+
+#result = multiply(x,y)
+#result = power(x,y)
+# result =Calculator.divide(x,y)
+
+# print("result: ",result)
